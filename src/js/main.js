5443810feeb42efafbba8c576ef1506245d46a88
Nikola Krivokapic <nikolakrivokapic84@gmail.com>
2018-12-10 03:30:22 +0100

    fix html and css

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom"
 import App from "./components/app"
 import AppStore from "./components/app/AppStore"
 
-const app = document.getElementById("app")
+const app = document.getElementById("app");
 
-ReactDOM.render(<App store={AppStore}/>, app)
+ReactDOM.render(<App store={AppStore}/>, app);
 
