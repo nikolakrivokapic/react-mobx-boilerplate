c86ae7ba5716bfb19232c204bf2a64b9cb87b245
Nikola Krivokapic <nikolakrivokapic84@gmail.com>
2018-12-10 03:21:43 +0100

    capi

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,25 @@
-import "../css/main.css"
-import React from "react"
-import ReactDOM from "react-dom"
+import "../css/main.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from "./components/app"
-import AppStore from "./components/app/AppStore"
+import App from "./components/app";
+import AppStore from "./components/app/AppStore";
+import {observe} from "mobx";
+import capiConfig from './config.json';
 
-const app = document.getElementById("app")
+const app = document.getElementById("app");
+const {CapiAdapter} = window.simcapi;
+let capiModel = new CapiAdapter.CapiModel({bgColor: ''});
 
-ReactDOM.render(<App store={AppStore}/>, app)
 
+ReactDOM.render(<App store={AppStore}/>, app);
+
+
+CapiAdapter.expose('bgColor', capiModel, capiConfig.capi.bgColor);
+console.log('CAPIADAPTER', CapiAdapter, capiConfig.capi.bgColor);
+const disposer2 = observe(AppStore, 'bgColor', (change) => {
+    console.log("bgColor changed to ", AppStore['bgColor']);
+
+        // this.syncToCapiModel();
+    capiModel.set('bgColor', AppStore['bgColor']);
+});
